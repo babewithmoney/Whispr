7994698f8124000214129414d73b0adc1c585e52
ThamimTommy-stitchflow <thamim.tommy@stitchflow.io>
2025-04-14 16:06:52 +0530

    feat: Enhance reaction handling with improved user feedback and flash message rendering

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -7,9 +7,17 @@ class ReactionsController < ApplicationController
     respond_to do |format|
       if @reaction.save
         format.turbo_stream
-        format.html { redirect_to root_path, notice: 'Reaction added!' }
+        format.html { redirect_to root_path, notice: 'Thanks for reacting! 🎉' }
       else
-        format.html { redirect_to root_path, alert: @reaction.errors.full_messages.join(', ') }
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.update("flash", 
+            partial: "shared/flash", 
+            locals: { 
+              message: "You've already shared your reaction to this confession! 😊", 
+              type: "alert" 
+            })
+        end
+        format.html { redirect_to root_path, alert: "You've already shared your reaction to this confession! 😊" }
       end
     end
   end
